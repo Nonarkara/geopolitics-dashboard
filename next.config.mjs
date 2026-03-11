b43b735bfe044bfca00cba061f535d10e7283bc2
Nonarkara <nonarkara@users.noreply.github.com>
2026-03-11 15:09:08 +0700

    Use JS Next config for Render builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   env: {
     NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN:
       process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN ??
